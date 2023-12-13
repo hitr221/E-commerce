f7a084c243be30e614a951d12dba63fec8e9e40c
hitr221 <2019-3-60-129@std.ewubd.edu>
2023-12-13 23:39:59 +0600

    Admin Product added

--- routes/web.php
+++ routes/web.php
@@ -91,6 +91,15 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::get('/unit/edit/{id}', [UnitController::class, 'edit'])->name('unit.edit');
     Route::post('/unit/update/{id}', [UnitController::class, 'update'])->name('unit.update');
 
+    Route::get('/product/add', [ProductController::class, 'index'])->name('product.add');
+    Route::get('/product/get_subcategory_by_category', [ProductController::class, 'getSubCategoryByCategory'])->name('product.get_subcategory_by_category');
+    Route::get('/product/manage', [ProductController::class, 'manage'])->name('product.manage');
+    Route::post('/product/new', [ProductController::class, 'create'])->name('product.new');
+    Route::get('/product/detail/{id}', [ProductController::class, 'detail'])->name('product.detail');
+    Route::get('/product/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::post('/product/update/{id}', [ProductController::class, 'update'])->name('product.update');
+    Route::get('/product/delete/{id}', [ProductController::class, 'delete'])->name('product.delete');
+
     
 });
 
